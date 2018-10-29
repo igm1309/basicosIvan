c79423a5031915a9a57e3a85fb5b2b7878413029
Iván Giner Medina <Ivn Giner Medina@192.168.123.135>
2018-10-29 19:09:27 +0100

    Creamos metodo logicos

--- src/basicosivan/BasicosIvan.java
+++ src/basicosivan/BasicosIvan.java
@@ -15,7 +15,31 @@ public class BasicosIvan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        logicos();
     }
     
+     /**
+     * Método para mostrar ejemplos de tipos logicos.
+     */
+    public static void logicos() {
+
+        //Estos son los tipos básicos
+       boolean es_igual;
+       int numero1; //numero creado para la comparación
+       int numero2; //numero creado para la comparación
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+       es_igual = true;
+       numero1 = 5;
+       numero2 = 7;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.println("Comparación de dos números: ");
+        System.out.println("El primer número es: "+numero1);
+        System.out.println("El segundo número es: "+numero2);
+        
+        if(numero1 == numero2){
+            System.out.println("Los números son IGUALES!!!");
+        }else{System.out.println("Los números NO son IGUALES");} 
+    }
 }
