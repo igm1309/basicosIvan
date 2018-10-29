78298cb0b8c32ae7cc89067884f919829c6c4860
Iván Giner Medina <Ivn Giner Medina@192.168.123.135>
2018-10-29 18:50:00 +0100

    Creamos metodo numericos

--- src/basicosivan/BasicosIvan.java
+++ src/basicosivan/BasicosIvan.java
@@ -15,7 +15,30 @@ public class BasicosIvan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+           numericos();
     }
     
+     /**
+     * Método para mostrar ejemplos de tipos numericos.
+     */
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    } 
 }
